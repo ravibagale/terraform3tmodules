4316bde6d6a95010eb233b297ad17c943891332a
ravibagale <30994679+ravibagale@users.noreply.github.com>
2023-02-15 18:12:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
 provider "aws" {
 region= "ap-south-1"
+access_key = var.accesskey
+secret_key = var.secretkey
 }
 
 
